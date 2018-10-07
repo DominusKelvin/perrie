d1f743d048b303a51eab04ee95de1db8c35afdec
Kelvin Omereshone <kelvinomereshone@gmail.com>
2018-10-07 19:43:26 +0100

    removed storage integration

--- content.js
+++ content.js
@@ -2,22 +2,11 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
   if (request.message === "clicked_browser_action") {
 
-    chrome.storage.sync.set(
-      {
-        monochromacy: '-moz-filter: grayscale(100%); -webkit-filter: grayscale(100%); filter: gray; filter: grayscale(100%)'
-      }, function() {
-      
-      let page = document.getElementsByTagName("html");
+    let page = document.getElementsByTagName("html");
 
-      chrome.storage.sync.get('monochromacy', function(data){
+    const monochromacy = '-moz-filter: grayscale(100%); -webkit-filter: grayscale(100%); filter: gray; filter: grayscale(100%)';
 
-      page[0].style.cssText +=  data.monochromacy;
-
-      })
-      
-    });
-
-    
+    page[0].style.cssText +=  monochromacy;
     
   }
 })
